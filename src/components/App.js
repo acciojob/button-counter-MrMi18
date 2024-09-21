4e22934943dcbaeb379fe53aca124b810d37c1c5
Mr Mi <is8388508@gmail.com>
2024-09-21 22:46:26 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [count,setCount] = useState(0)
+  function increaseCount(e){
+    
+    setCount(count+1)
+    
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        {/* Do not remove the main div */   }
+        
+        <p>Button clicked {count} times</p>
+        <button onClick={increaseCount} > Click Me </button>
     </div>
   )
 }
